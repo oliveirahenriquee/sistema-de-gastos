e78d643ca4965c814ee1a3abc7349c057d43eedc
oliveirahenriquee <henriq_cps@hotmail.com>
2026-04-06 21:49:16 -0300

    Ajuste de porta e SSL para o Aiven

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
@@ -7,10 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '', // Fica vazio ou usa variável
-    database: process.env.DB_NAME || 'planilha_db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT, // Adicione esta linha!
+    ssl: { rejectUnauthorized: false } // Adicione esta linha para o Aiven aceitar
 });
 
 db.connect(err => {
